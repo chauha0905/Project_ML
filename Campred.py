3ceb408f62e82e54c238a7273583280fcf6c7c2a
chauha0905 <chauha0905@gmail.com>
2021-11-21 00:58:56 +0700

    Update Campred.py

--- Campred.py
+++ Campred.py
@@ -12,7 +12,7 @@ choice = st.sidebar.selectbox('Side Menu?', menu)
 
 #Load your model and check create the class_names list
 # Model_Path = 'my_model_save.h5'
-model = tf.keras.models.load_model('my_model_save')
+model = tf.keras.models.load_model('my_model_save.h5')
 class_names = ['1000', '2000', '5000', '10000', '20000', '50000', '100000', '200000', '500000']
 
 model.compile(optimizer='adam',
