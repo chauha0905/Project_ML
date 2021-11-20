e51ecc5fbf336ab554b5e3a5ed3259baca7fe41a
chauha0905 <chauha0905@gmail.com>
2021-11-21 00:06:35 +0700

    Update Campred.py

--- Campred.py
+++ Campred.py
@@ -13,7 +13,7 @@ choice = st.sidebar.selectbox('Side Menu?', menu)
 #Load your model and check create the class_names list
 Model_Path = 'my_model_save.h5'
 class_names = ['1000', '2000', '5000', '10000', '20000', '50000', '100000', '200000', '500000']
-model = tf.keras.load_model(Model_Path)
+model = tf.keras.models.load_model(Model_Path)
 
 
 if choice == 'Capture From Webcam':
