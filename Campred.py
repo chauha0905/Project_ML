2e477f7bf1b1c7e531bdf57d0160062748599b8e
chauha0905 <chauha0905@gmail.com>
2021-11-21 00:34:25 +0700

    Update Campred.py

--- Campred.py
+++ Campred.py
@@ -17,7 +17,7 @@ class_names = ['1000', '2000', '5000', '10000', '20000', '50000', '100000', '200
 
 model.compile(optimizer='adam',
               loss='categorical_crossentropy',
-              metrics=['accuracy']))
+              metrics=['accuracy'])
 
 if choice == 'Capture From Webcam':
     cap = cv2.VideoCapture(0)  # device 0
