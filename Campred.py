cb6487fb25faae08259d0d48d074d7967dd40e21
chauha0905 <chauha0905@gmail.com>
2021-11-21 00:23:47 +0700

    Update Campred.py

--- Campred.py
+++ Campred.py
@@ -49,7 +49,7 @@ if choice == 'Capture From Webcam':
         st.write('Image is capture:')
 
         #Resize the Image according with your model
-        captured_image = cv2.resize(224, 224)
+        captured_image = cv2.resize(224, 224, 3)
         #Expand dim to make sure your img_array is (1, Height, Width , Channel ) before plugging into the model
         img_array  = np.expand_dims(captured_image, axis=0)
         #Check the img_array here
