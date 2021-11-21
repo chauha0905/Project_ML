415e8023862f1cbd99b8331645b53d2aaefc37c3
chauha0905 <chauha0905@gmail.com>
2021-11-21 17:18:43 +0700

    updated

--- Campred_test.py
+++ Campred_test.py
@@ -55,9 +55,9 @@ if choice == 'Capture From Webcam':
         #Expand dim to make sure your img_array is (1, Height, Width , Channel ) before plugging into the model
         img_array  = np.expand_dims(captured_image, axis=0)
         #Check the img_array here
-        st.write(img_array)
+        # st.write(img_array)
 
-        prediction = model.predict(img_array)
+        model.predict(img_array)
 
         # Preprocess your prediction , How are we going to get the label name out from the prediction
         # Now it's your turn to solve the rest of the code
--- streamlit_wp8.py
+++ streamlit_wp8.py
@@ -1,4 +1,5 @@
-import streamlit as st
+
+import streamlit as st 
 import pandas as pd
 import cv2
 import numpy as np 
