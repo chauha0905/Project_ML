d494ec6861fcbc2fc56df2ea5728ab97a038688d
chauha0905 <chauha0905@gmail.com>
2021-11-22 15:29:01 +0700

    Update damlakeo.py

--- damlakeo.py
+++ damlakeo.py
@@ -5,7 +5,7 @@ menu = ['Rock, paper, scissors']
 choice = st.sidebar.selectbox('Homebox', menu)
 
 if choice == 'Rock, paper, scissors':
-    player = input("Fill your option: ")
+    player = st.text_input("Fill your option: ")
     computer = randint(0,2)
     # Rock, paper, scissors, shoot
     if computer == 0:
@@ -14,31 +14,30 @@ if choice == 'Rock, paper, scissors':
         computer = "Paper"
     else:
         computer = "Scissors"
-    print(computer)
+    st.write(computer)
 
-    print("\n---")
-    print("You choose: " + player)
-    print("Computer chooses: " + str(computer))
+    st.text("\n---")
+    st.write("You choose: " + player)
+    st.write("Computer chooses: " + str(computer))
 
     if player == computer:
-        print("Draw")
+        st.write("Draw")
     else:
         if player == "Rock" or player == "rock" or player == "ROCK" or player == "r" or player == "R":
             if computer == "Paper":
-                print("Lose")
+                st.write("Lose")
             else:
-                print("Win")
+                st.write("Win")
         elif player == "Paper" or player == "P" or player == "paper" or player == "p" or player == "PAPER":
             if computer == "Rock":
-                print("Win")
+                st.write("Win")
             else:
-                print("Lose")
+                st.write("Lose")
         elif player == "Scissors" or player == "SCISSORS" or player == "S" or player == "s":
             if computer == "Rock":
-                print("Lose")
+                st.write("Lose")
             else:
-                print("Win")
+                st.write("Win")
         else:
-            print("Wrong input! Please retype!")
+            st.write("Wrong input! Please retype!")
         
-    print("\n---")
