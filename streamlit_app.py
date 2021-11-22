5cf6e58fb2299ec7e7eed4987e8c8bd252046899
chauha0905 <chauha0905@gmail.com>
2021-11-22 16:34:30 +0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,43 +42,55 @@ if choice == 'The playground':
         st.write(your_name,' is a beautiful name. Have a good day!')
 
 elif choice == 'Childhood game':
-    st.title('Childhood is a priceless treasure')
+    st.title('Childhood is a priceless treasure.')
     st.text("What was your childhood's memory?")
-    st.image()
-    player = st.text_input("Fill your choice: ")
-    computer = np.random.randint(0,2)
-   
-    if computer == 0:
-        computer = "Rock"
-    if computer == 1:
-        computer = "Paper"
-    else:
-        computer = "Scissors"
+    st.text('')
+    st.text('')
 
-    st.text("")
-    st.write("You choose: ", player)
-    st.write("Computer chooses: ", computer)
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image('media/Childhood_game.png')
 
-    if player == computer:
-        st.write("Draw")
-    else:
-        if player == "Rock" or player == "rock" or player == "ROCK" or player == "r" or player == "R":
-            if computer == "Paper":
-                st.write("You lose!")
-            else:
-                st.write("You win!")
-        elif player == "Paper" or player == "P" or player == "paper" or player == "p" or player == "PAPER":
-            if computer == "Rock":
-                st.write("You win!")
-            else:
-                st.write("You lose!")
-        elif player == "Scissors" or player == "SCISSORS" or player == "S" or player == "s":
-            if computer == "Rock":
-                st.write("You lose!")
-            else:
-                st.write("You win!")
+    with col2:
+        st.header('Game')
+        st.write('Rock. Paper. Scissors. SHOOT')
+        st.text("Intro: This is a game named 'đấm-lá-kéo'.")
+        st.text("Tips: Enter your choice by words: Rock/rock/ROCK/r/R.")
+
+        player = st.text_input("Fill your choice: ")
+        computer = np.random.randint(0,2)
+    
+        if computer == 0:
+            computer = "Rock"
+        if computer == 1:
+            computer = "Paper"
         else:
-            st.write("Wrong input! Please retype!")
+            computer = "Scissors"
+
+        st.text("")
+        st.write("You choose: ", player)
+        st.write("Computer chooses: ", computer)
+
+        if player == computer:
+            st.write("Draw")
+        else:
+            if player == "Rock" or player == "rock" or player == "ROCK" or player == "r" or player == "R":
+                if computer == "Paper":
+                    st.write("You lose!")
+                else:
+                    st.write("You win!")
+            elif player == "Paper" or player == "P" or player == "paper" or player == "p" or player == "PAPER":
+                if computer == "Rock":
+                    st.write("You win!")
+                else:
+                    st.write("You lose!")
+            elif player == "Scissors" or player == "SCISSORS" or player == "S" or player == "s":
+                if computer == "Rock":
+                    st.write("You lose!")
+                else:
+                    st.write("You win!")
+            else:
+                st.write("Wrong input! Please retype!")
 
 
 elif choice == 'Entertainment':
