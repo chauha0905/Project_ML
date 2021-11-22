d913fd7ab9c8586d6f7876c08637d6d9f2ac19de
chauha0905 <chauha0905@gmail.com>
2021-11-23 00:14:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,20 +5,20 @@ import cv2
 import numpy as np 
 import matplotlib as plt 
 import tensorflow as tf 
-from datetime import datetime
 
 st.set_page_config(layout='centered', initial_sidebar_state='auto')
 
-# Add model here
+# Add prediction model _ VND Banknotes
 Model_Path = 'my_model_save.h5'
 class_names = ['1000', '10000', '100000', '2000', '20000', '200000', '5000', '50000', '500000']
 model = tf.keras.models.load_model(Model_Path)
 
+# Layout the streamlit app
 menu = ['The playground', 'Childhood game', 'PicPred', 'CamPred', 'Entertainment']
 choice = st.sidebar.selectbox('Homebox', menu)
 
 with st.sidebar:
-    st.text('')
+    st.text('') 
     st.text('')
     st.text('')
     st.text('')
@@ -26,14 +26,14 @@ with st.sidebar:
     st.text('') 
     st.image('media/Playground.gif')
 
-    # Add in datetime in sidebar
+    # Add in date & time in sidebar
     st.date_input('Today')
     st.time_input('Current time')
 
+# Start decor and build content
 if choice == 'The playground':
     st.title("T h e p l a y g r o u n d")
     st.text('Leave your signs after visiting!')
-    
     st.text('')
     st.image('media/2.png')
     st.subheader('Hi there.')
@@ -45,18 +45,16 @@ elif choice == 'Childhood game':
     st.title('Childhood is a priceless treasure.')
     st.text("What was your childhood's memory?")
     st.image('media/game3.png')
-    st.text('')
-    st.text('')
     st.header('Rock. Paper. Scissors. SHOOT!')
-
+    
     col1, col2 = st.columns([1.5, 1])
     with col1:
-        st.text("Intro: This is a game named 'đấm-lá-kéo'.")
+        st.text("This is a game named 'đấm-lá-kéo'.")
         st.text("Tips: Enter your choice by words:")
         st.text("* Rock/rock/ROCK/r/R")
         st.text("* Paper/paper/PAPER/p/P")
         st.text("* Scissors/scissors/SCISSORS/s/S")
-
+    # Game content
     with col2:
         player = st.text_input('Fill your choice & Enter')
         computer = np.random.randint(0,3)
@@ -93,10 +91,8 @@ elif choice == 'Childhood game':
             else:
                 st.write("Wrong input! Please retype!")
 
-
 elif choice == 'Entertainment':
     st.title('Enjoy your way!')
-
     col1, col2 = st.columns(2)
     with col1:
         st.image('media/Thumb2.gif')
@@ -116,22 +112,17 @@ elif choice == 'Entertainment':
         video_file = open('media/Believer - Cover.mp4', 'rb')
         video_bytes = video_file.read()
         st.video(video_bytes)
-        
         st.text('Believer_Cover by Kids')
     with col2:
-
         st.video('media/Imagine Dragons - Natural.mp4')
         st.text('Natural_Imagine Dragons')
     st.text('')
-
     st.text('(Sources: YouTube & Internet)')
    
-
 elif choice == 'CamPred':
     st.title('Play fun!')
 
     cap = cv2.VideoCapture(0)  # device 0
-
     st.subheader('Take a photo of banknotes, please!')
     st.text('(Using Vietnamese dong only)')
     run = st.checkbox('Show Webcam')
@@ -155,38 +146,35 @@ elif choice == 'CamPred':
             break
 
     cap.release()
-
+    # Processing & Prediction
     if  captured_image.all() != None:
-            # st.write('Image is captured:')
-            
         captured_image = cv2.resize(captured_image, (224,224))  #Resize Image according to model
-        img_array  = np.expand_dims(captured_image, axis=0)     #Resize Image according to model
-        prediction = model.predict(img_array)
-        index = np.argmax(prediction.flatten())
+        img_array  = np.expand_dims(captured_image, axis=0)     #Expand dim
+        prediction = model.predict(img_array)                   #Prediction by array
+        index = np.argmax(prediction.flatten())                 
         st.write("Answer: It's", class_names[index], "VND")
         st.write("The predictions can not work well sometimes. Don't be serious. JUST FOR FUN!")
 
-    st.success('Status: In progress')
-
+    st.success('Application status: In progress')
 
 elif choice == 'PicPred':
     st.title('Such a rich people!')
     st.image('media/Thumb3.png')
-
     st.text("")
     st.subheader('Upload your money')
     st.text('(Using Vietnamese dong only)')
 
     photo_uploaded = st.file_uploader('',['png', 'jpeg', 'jpg'])
-
+    # Processing image & prediction
     if photo_uploaded != None:
         image_np = np.asarray(bytearray(photo_uploaded.read()), dtype=np.uint8)
         img = cv2.imdecode(image_np, 1)
         st.image(img, channels='BGR')
-        img_resized = cv2.resize(img, (224,224))                
+        img_resized = cv2.resize(img, (224,224))                #Resize Image according to model
         img_array  = np.expand_dims(img_resized, axis=0)        #Expand dim
         prediction = model.predict(img_array)                   #Prediction
         index = np.argmax(prediction.flatten())
         st.write("Answer: It's", class_names[index], "VND")
         st.write("The predictions can not work well sometimes. Don't be serious. JUST FOR FUN!")
-
+    
+# Thank you!
